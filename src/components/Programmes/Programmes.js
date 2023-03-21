cfd447f7f49823063db2bcb1de74eeef0d7503dc
David Isidore <isidoredavid@protonmail.com>
2023-03-21 22:59:00 +0100

    Mise  en place du CSS du composant Programmes

--- src/components/Programmes/Programmes.js
+++ src/components/Programmes/Programmes.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import './programmes.scss'
+import prog1 from '../../assets/prog1.jpg'
+import prog2 from '../../assets/prog2.jpg'
+import prog3 from '../../assets/prog3.jpg'
 
 export default function Programmes() {
     return (
         <div className="programmes">
             <h2>Mes programmes</h2>
-            <div></div>
+            <div>
+                <div className="prog">
+                    <img src={prog1} alt="prog1" className="prog_photo" />{' '}
+                </div>
+                <div>
+                    <img src={prog2} alt="prog2" className="prog_photo" />{' '}
+                </div>
+                <div>
+                    <img src={prog3} alt="prog3" className="prog_photo" />{' '}
+                </div>
+            </div>
         </div>
     )
 }
