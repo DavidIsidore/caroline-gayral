3d801b26d19d63b7676518358c2f7dd622ae5e91
David Isidore <isidoredavid@protonmail.com>
2023-03-21 22:41:55 +0100

    Mise en place du composant Programmes

---
+++ src/components/Programmes/Programmes.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Programmes() {
+    return (
+        <div>
+            <h2>Mes programmes</h2>
+            <div></div>
+        </div>
+    )
+}
