18c1ea8dae61a6381fcef16dfff700f74ae5b9a8
David Isidore <isidoredavid@protonmail.com>
2023-03-21 14:54:41 +0100

    Mise en place des éléments de la navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import logo from '../../assets/logo.jpg'
+import Navbar from '../Navbar/Navbar'
 import './header.scss'
 
 export default function Header() {
     return (
         <div className="header">
             <img src={logo} alt="logo" className="header_logo" />
+            <Navbar />
         </div>
     )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 export default function Navbar() {
-    return <nav></nav>
+    const location = useLocation
+    return (
+        <nav className="nav">
+            <ul className="nav_list">
+                <li
+                    className={
+                        location.pathname === '/'
+                            ? 'nav_list_item_active'
+                            : 'nav_list_item'
+                    }
+                >
+                    <Link to="/">Accueil</Link>
+                </li>
+            </ul>
+        </nav>
+    )
 }
