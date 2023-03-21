ed68fbff4faaeae7b7a7a70186c083303bca5a8a
David Isidore <isidoredavid@protonmail.com>
2023-03-21 12:00:04 +0100

    Ajout des logos du footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import julie from '../../assets/logo-julie-herve.png'
+import eveil from '../../assets/logo-eveil.webp'
+
+export default function Footer() {
+    return (
+        <div>
+            <div>
+                <img src={julie} alt="logo julie herve" />
+                <img src={eveil} alt="logo eveil" />
+            </div>
+        </div>
+    )
+}
