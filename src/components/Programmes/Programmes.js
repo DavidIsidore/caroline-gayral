7a8b122c9f228fabe2e3eefbb7dde7c91fcd022d
David Isidore <isidoredavid@protonmail.com>
2023-03-22 10:43:20 +0100

    Ajout du texte des programmes

--- src/components/Programmes/Programmes.js
+++ src/components/Programmes/Programmes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './programmes.scss'
-import prog1 from '../../assets/prog1.jpg'
+import prog1 from '../../assets/prog11.jpg'
 import prog2 from '../../assets/prog2.jpg'
 import prog3 from '../../assets/prog3.jpg'
 
@@ -11,12 +11,15 @@ export default function Programmes() {
             <div className="prog">
                 <div>
                     <img src={prog1} alt="prog1" className="prog_photo" />{' '}
+                    <p className="prog_txt">Coaching: Changer de fréquence</p>
                 </div>
                 <div>
                     <img src={prog2} alt="prog2" className="prog_photo" />{' '}
+                    <p className="prog_txt">Hypnose régressive: Reconnexion</p>
                 </div>
                 <div>
                     <img src={prog3} alt="prog3" className="prog_photo" />{' '}
+                    <p className="prog_txt">Sophrologie / Hypnose</p>
                 </div>
             </div>
         </div>
