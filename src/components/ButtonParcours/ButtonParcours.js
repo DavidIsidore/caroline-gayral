d67224655ca61a0d30bedb1d100dbbefe96ed30b
David Isidore <isidoredavid@protonmail.com>
2023-04-03 17:01:19 +0200

    Implémentation du compsant Bouton Parcours

---
+++ src/components/ButtonParcours/ButtonParcours.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './buttonParcours.scss'
+
+export default function ButtonParcours() {
+    return (
+        <button className="buttonParcours">
+            {' '}
+            Plus sur mon parcours en cliquant ici
+        </button>
+    )
+}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Header from '../../components/Header/Header'
 import Programmes from '../../components/Programmes/Programmes'
 import Footer from '../../components/Footer/Footer'
-import portrait from '../../assets/portrait.jpg'
 import './home.scss'
 import Presentation from '../../components/Presentation/Presentation'
 import PresentationBloc1 from '../../components/PresentationBloc1/PresentationBloc1'
 import PresentationBloc2 from '../../components/PresentationBloc2/PresentationBloc2'
+import ButtonParcours from '../../components/ButtonParcours/ButtonParcours'
 
 export default function Home() {
     return (
@@ -16,12 +16,10 @@ export default function Home() {
 
             <PresentationBloc1 />
             <PresentationBloc2 />
-            <div className="text">
-                <button className="button">
-                    Plus sur mon parcours en cliquant ici
-                </button>
-                <Programmes />
-            </div>
+            <ButtonParcours />
+
+            <Programmes />
+
             <Footer />
         </main>
     )
